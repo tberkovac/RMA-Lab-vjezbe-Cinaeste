1d616f9c5f1a38e4725d6391917211f3a796ed6a
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-03-29 15:30:05 +0200

    Zadatak 1

---
+++ app/src/main/java/com/example/lv1drugiput/view/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lv1drugiput.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lv1drugiput.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
