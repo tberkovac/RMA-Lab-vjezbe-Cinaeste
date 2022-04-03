74d35f19da28beffe6b0b542b630c8d614153c71
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-04-03 21:16:48 +0200

    Zadatak 4

--- app/src/main/java/com/example/lv1drugiput/MainActivity.kt
+++ app/src/main/java/com/example/lv1drugiput/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.lv1drugiput
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lv1drugiput.data.Movie
@@ -11,6 +13,7 @@ import com.example.lv1drugiput.viewmodel.MovieListViewModel
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var searchText: EditText
     private lateinit var favoriteMovies: RecyclerView
     private lateinit var recentMovies: RecyclerView
     private lateinit var favoriteMoviesAdapter: MovieListAdapter
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         favoriteMovies = findViewById(R.id.favoriteMovies)
         recentMovies = findViewById(R.id.recentMovies)
+        searchText = findViewById(R.id.searchText)
         favoriteMovies.layoutManager = LinearLayoutManager(
             this,
             LinearLayoutManager.HORIZONTAL,
@@ -42,13 +46,22 @@ class MainActivity : AppCompatActivity() {
         recentMovies.adapter = recentMoviesAdapter
         favoriteMoviesAdapter.updateMovies(movieListViewModel.getFavoriteMovies())
         recentMoviesAdapter.updateMovies(movieListViewModel.getRecentMovies())
+
+        if(intent?.action == Intent.ACTION_SEND && intent?.type == "text/plain")
+            handleSendText(intent)
+    }
+
+    private fun handleSendText(intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            searchText.setText(it)
+        }
     }
 
     private fun showMovieDetails(movie: Movie) {
         val intent = Intent(this, MovieDetailActivity::class.java).apply {
             putExtra("movie_title", movie.title)
+            Log.v("Intent ", "klik na film za detalje")
         }
         startActivity(intent)
     }
-
 }
