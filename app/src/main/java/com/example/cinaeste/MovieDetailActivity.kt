4a37218a901444755f0ab754e54c6665cbca3e85
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-04-03 23:01:37 +0200

    ZSR otvara se yt

--- app/src/main/java/com/example/cinaeste/MovieDetailActivity.kt
+++ app/src/main/java/com/example/cinaeste/MovieDetailActivity.kt
@@ -4,14 +4,16 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cinaeste.data.Movie
 import com.example.cinaeste.viewmodel.MovieDetailViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MovieDetailActivity : AppCompatActivity() {
+
     private var movieDetailViewModel =  MovieDetailViewModel()
     private lateinit var movie: Movie
     private lateinit var title : TextView
@@ -20,18 +22,30 @@ class MovieDetailActivity : AppCompatActivity() {
     private lateinit var genre : TextView
     private lateinit var website : TextView
     private lateinit var poster : ImageView
+    private lateinit var shareButton : FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
         title = findViewById(R.id.movie_title)
         overview = findViewById(R.id.movie_overview)
         releaseDate = findViewById(R.id.movie_release_date)
         genre = findViewById(R.id.movie_genre)
         poster = findViewById(R.id.movie_poster)
         website = findViewById(R.id.movie_website)
+        shareButton = findViewById(R.id.shareButton)
+
+        website.setOnClickListener{
+            showWebsite()
+        }
+        title.setOnClickListener{
+            youtubeSearch()
+        }
         val extras = intent.extras
+
         if (extras != null) {
-            movie = movieDetailViewModel.getMovieByTitle(extras.getString("movie_title",""))
+            movie = movieDetailViewModel.getMovieByTitle(extras.getString("movie_title", ""))
             populateDetails()
         } else {
             finish()
@@ -42,19 +56,17 @@ class MovieDetailActivity : AppCompatActivity() {
         releaseDate.text=movie.releaseDate
         genre.text=movie.genre
         website.text=movie.homepage
-        website.setOnClickListener(){
-            showWebiste();
-        }
         overview.text=movie.overview
         val context: Context = poster.context
         var id: Int = context.resources
             .getIdentifier(movie.genre, "drawable", context.packageName)
-        if (id===0) id=context.resources
+        if (id==0) id=context.resources
             .getIdentifier("picture1", "drawable", context.packageName)
         poster.setImageResource(id)
     }
 
-    private fun showWebiste() {
+    private fun showWebsite(){
+
         val webIntent: Intent = Uri.parse(movie.homepage).let { webpage ->
             Intent(Intent.ACTION_VIEW, webpage)
         }
@@ -65,5 +77,16 @@ class MovieDetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun youtubeSearch(){
+        val intent = Intent(Intent.ACTION_SEARCH).apply {
+            setPackage("com.google.android.youtube")
+            putExtra("query", movie.title + " trailer")
+        }
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            // Definisati naredbe ako ne postoji aplikacija za navedenu akciju
+        }
+    }
 
 }
